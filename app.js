9aa0de24fda291e3d1915fc50c04675ab585af7b
Nick Olds <nolds9@yahoo.com>
2016-07-18 15:46:14 -0400

    can get complement

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ if(fs.existsSync(__dirname + "/new.json")) {
   if(process.env.environment === "test") {
     old.shift()
   }
-  fs.writeFileSync(__dirname + "/old.json", old, "utf8");
+  fs.writeFileSync(__dirname + "/old.json", JSON.stringify(old), "utf8");
 } else {
   p.listings(function (data) {
-    fs.writeFileSync(__dirname + "/new.json", data, "utf8");
+    fs.writeFileSync(__dirname + "/new.json", JSON.stringify(data), "utf8");
     process.exit();
   })
 }
 
 p.listings(function (newListings) {
   p.complement(old.reverse(), newListings.reverse(), function (complement) {
-    console.log(complement);
+    console.log(complement);  // TODO: make twitter post with complement data
   })
 })
