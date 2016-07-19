5f72dd3d0520eaa995df52603967d3b81d3b0810
Nick Olds <nolds9@yahoo.com>
2016-07-19 15:24:50 -0400

    can upload and tweet image

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var env = require("./env.js");
 var Twitter = require("twitter")
 var p = page("https://generalassemb.ly/education?where=washington-dc");
 var base64 = require('node-base64-image');
+var client = new Twitter(env);
 
 if(fs.existsSync(__dirname + "/new.json")) {
   var old = JSON.parse(fs.readFileSync(__dirname + "/new.json", "utf8"));
@@ -27,21 +28,20 @@ p.listings(function (newListings) {
 function tweet (c) {
   if(c.title && c.date_description && c.time_description && c.url && c.image_url) {
     var newStatus = c.title +  " " + c.date_description + " " + c.time_description + " " + c.url
-    var client = new Twitter(env)
     var image = base64.encode(c.image_url, {}, function (err, response) {
       if(err) console.log(err);
       var data = response.toString('base64')
       client.post('/media/upload.json', {media_data: data}, function (err, response) {
-        if(err)console.log( err);
-        console.log(response);
-        if(response.media_id) {
-          var client2 = new Twitter(env)
-          client2.post("statuses/update.json", {status: newStatus, media_id: [response.media_id]}, function (err, tweet, response) {
-            if (err) console.log( err)
+        if(!err && response.media_id_string) {
+          client.post('/statuses/update.json', {media_ids: response.media_id_string, status: newStatus}, function(err, tweet, response) {
+            if(err) {
+              console.log(err)
+            } else {
+              console.log(tweet);
+            }
           })
         }
       })
-
     })
   }
 }
